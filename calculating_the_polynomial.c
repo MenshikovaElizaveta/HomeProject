86a1027877605b0fc6f9e8533ec9243ae52f5d38
Elizaveta M <mefmag01@gmail.com>
2025-12-10 14:51:45 +0000

    две переменные отличаются теперь не только регистром

--- calculating_the_polynomial.c
+++ calculating_the_polynomial.c
@@ -1,9 +1,9 @@
 include <stdio.h>
 
 int main(void){
-    int x, X;
+    int x, a;
     printf("Введите число: ");
     scanf("%d", &x);
-    X=x*x;
-    printf("%d\n", (X+x)*(X+1)+1);
+    a=x*x;
+    printf("%d\n", (a+x)*(a+1)+1);
 }
