24e740c57ab185cc3a66eff0369c662759ee0b0e
Elizaveta M <mefmag01@gmail.com>
2025-12-10 14:36:48 +0000

    Убрана ненужная проверка и лишний цикл

--- Tickets.c
+++ Tickets.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int a, b, c, i;
-    int quantity = 1;
-    int counter[27] = {0};
+    int quantity = 0;
+    int counter[28] = {0};
     
-    for (i = 0; i < 27; ++i) {
-        for (a = 0; a < 10; ++a) {
-            for (b = 0; b < 10; ++b) {
-                for (c = 0; c < 10; ++c) {
-                    if ((a + b + c) == i) {
-                        ++counter[i];
-                    }
-                }
+    for (int a = 0; a < 10; ++a) {
+        for (int b = 0; b < 10; ++b) {
+            for (int c = 0; c < 10; ++c) {
+                ++counter[a + b + c];
             }
         }
+    }
+
+    for( int i = 0; i < 28; i++){
         quantity = quantity + counter[i] * counter[i];
     }
     
