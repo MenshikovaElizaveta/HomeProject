4b542dd3de2701025370301cf231e45922861302
Elizaveta M <mefmag01@gmail.com>
2025-12-12 17:15:08 +0000

    Добавлен файл, с программой, считающей количество нулевых элементов в массиве

---
+++ numzeroinarr.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+int countZeroElements(int arr[], int size){
+    int k = 0;
+
+    for( int i = 0; i < size; i++){
+        if( arr[i] == 0){
+            k++;
+        }
+    }
+
+    return k;
+}
+
+
+int main(void){
+    int size;
+    printf("Введите длину массива: ");
+    scanf("%d", &size);
+    int arr[size];
+    printf("Введите %d чисел:\n", size);
+    for(int i = 0; i < size; i++){
+        scanf("%d", &arr[i]);
+    }
+    printf("массив содержит %dNumber_of_zeros нулевых элементов", countZeroElements(arr, size));
+    
+    return 0;
+}
