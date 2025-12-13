ee24e681d6919063e68ac94053d09f5e0dfbac44
Elizaveta M <mefmag01@gmail.com>
2025-12-13 21:10:03 +0000

    часть  кода написана только один раз

--- IncompleteSeparation.c
+++ IncompleteSeparation.c
@@ -2,52 +2,43 @@
 
 
 int main(void){
-    int A, a, B, b, count=0;
+    int A, a, B, b, sign = 1, count=0;
+
     printf("Введите два числа: ");
     scanf("%d %d", &a, &b);
+
     if (b==0){
         printf("Ошибка: деление на ноль.\n");
         return 1;
     }
-    if (a>0 && b>0){
-        A=a;
-        B=b;
-        count=0;
-        while(A>=B){
-            A=A-B;
-            count+=1;
-        }
+    
+    if ((a < 0 && b > 0) || (a > 0 && b < 0)) {
+        sign = -1;
     }
-    if (a>0 && b<0){
-        A=a;
-        B=b*(-1);
-        count=0;
-        while(A>=B){
-            A=A-B;
-            count+=1;
-        }
-        count*=-1;
-        if (A>0) count--;
+
+    A = a;
+    B = b;
+    count = 0;
+
+    if (A < 0){
+        A = -A;
     }
-    if (a<0 && b<0){
-        A=a*(-1);
-        B=b*(-1);
-        count=0;
-        while(A>=B){
-            A=A-B;
-            count+=1;
-        }
+    if (B < 0){
+        B = -B;
     }
-    if (a<0 && b>0){
-        A=a*(-1);
-        B=b;
-        count=0;
-        while(A>=B){
-            A=A-B;
-            count+=1;
-        }
-        count*=-1;
-        if (A>0) count--;
+
+    while(A >= B){
+        A = A - B;
+        count++;
     }
+
+    if (sign < 0 && A > 0){
+        count++;
+    }
+
+    count *= sign;
+
     printf("%d неполное частноe от деления %d на %d", count, a, b);
+
+    return 0;
 }
