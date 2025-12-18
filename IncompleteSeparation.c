9a8679b386b86513f5932ac5667d1815ea031bf1
Elizaveta M <mefmag01@gmail.com>
2025-12-18 10:12:25 +0000

    Исправлена проверка на знаки делимого, делителя и частного.

--- IncompleteSeparation.c
+++ IncompleteSeparation.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
 int main(void){
-    int A, a, B, b, sign = 1, count=0;
+    int A, a, B, b, count=0;
 
     printf("Введите два числа: ");
     scanf("%d %d", &a, &b);
@@ -11,32 +12,20 @@ int main(void){
         printf("Ошибка: деление на ноль.\n");
         return 1;
     }
-    
-    if ((a < 0 && b > 0) || (a > 0 && b < 0)) {
-        sign = -1;
-    }
-
-    A = a;
-    B = b;
-    count = 0;
-
-    if (A < 0){
-        A = -A;
-    }
-    if (B < 0){
-        B = -B;
-    }
+    A = abs(a);
+    B = abs(b);
 
     while(A >= B){
         A = A - B;
         count++;
     }
 
-    if (sign < 0 && A > 0){
-        count++;
+    if (a * b < 0){
+        count *= -1;
+        if (A != 0){
+            count--;
+        }
     }
-
-    count *= sign;
 
     printf("%d неполное частноe от деления %d на %d", count, a, b);
 
