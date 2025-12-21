fcd1aff0df111afa167ed8e72127c95fbd8c5af8
Elizaveta M <mefmag01@gmail.com>
2025-12-21 17:19:58 +0000

    Добавлен сортированный список

--- SortList.c
+++ SortList.c
@@ -1,15 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-    int *list = calloc(100, sizeof(int));
-    int size = 0;
-    char opcode;
 
-    if (list == NULL) {
+typedef struct Node {
+    int num;
+    struct Node* next;
+} Node;
+
+
+Node* createNode(int val) {
+    Node* new = malloc(sizeof(Node));
+    if (new == NULL) {
         printf("Ошибка выделения памяти!\n");
-        return 1;
+        return NULL;
     }
+    new->num = val;
+    new->next = NULL;
+    return new;
+}
+
+
+void put(Node** head, int val) {
+    Node* new = createNode(val);
+    if (new == NULL) {
+        return;
+    }
+    if (*head == NULL || (*head)->num >= val) {
+        new->next = *head;
+        *head = new;
+        return;
+    }
+    Node* cur = *head;
+    while (cur->next != NULL && cur->next->num < val) {
+        cur = cur->next;
+    }
+    new->next = cur->next;
+    cur->next = new;
+}
+
+
+void pop(Node** head, int val) {
+    if (*head == NULL) {
+        printf("Список пуст.\n");
+        return;
+    }
+    if ((*head)->num == val) {
+        Node* temp = *head;
+        *head = (*head)->next;
+        free(temp);
+        return;
+    }
+    Node* cur = *head;
+    Node* previous = NULL;
+    while (cur != NULL && cur->num != val) {
+        previous = cur;
+        cur = cur->next;
+    }
+    if (cur == NULL) {
+        printf("Такого числа в списке нет.\n");
+        return;
+    }
+    previous->next = cur->next;
+    free(cur);
+    return;
+}
+
+
+void printlist(Node* head) {
+    Node* cur = head;
+    while (cur != NULL) {
+        printf("%d ", cur->num);
+        cur = cur->next;
+    }
+}
+
+
+int main(void) {
+    Node* head = NULL;
+    int opcode, val;
     
     printf("Введите одну из следующих операций:\n");
     printf("0 - выйти\n");
@@ -17,73 +85,42 @@ int main(void) {
     printf("2 - удалить значение из списка\n");
     printf("3 - распечатать список\n");
     
-    opcode = getchar();
-    
-    while (opcode != '0') {
-        if (opcode == '1') {
+    do {
+        scanf("%d", &opcode);
+        while (getchar() != '\n');
+        switch (opcode) {
+        case 0:
+            break;
+        case 1:
             printf("Добавьте число в список: ");
-            scanf("%d", &list[size]);
-            getchar();
-            
-            size++;
-            int i = size - 1;
-            
-            while (i > 0 && list[i] < list[i - 1]) {
-                int temp = list[i - 1];
-                list[i - 1] = list[i];
-                list[i] = temp;
-                i--;
-            }
-            
-            i = size - 1;
-            printf("Введите следующую команду: ");
-            opcode = getchar();
-        } 
-        else if (opcode == '2') {
-            int del;
+            scanf("%d", &val);
+            while (getchar() != '\n');
+            put(&head, val);
+            break;
+        case 2:
             printf("Удалите число из списка: ");
-            scanf("%d", &del);
-            getchar();
-            
-            int num;
-            int found = 0;
-
-            for (int i = 0; i < size; i++) {
-                if (list[i] == del) {
-                    num = i;
-                    found = 1;
-                    break;
-                }
-            }
-            
-            if (found) {
-                for (int i = num; i < size - 1; i++) {
-                    list[i] = list[i + 1];
-                }
-                size--;
-            } 
-            else {
-                printf("Такого числа в списке нет.");
-            }
-
-            printf("Введите следующую команду: ");
-            opcode = getchar();
-        } 
-        else if (opcode == '3') {
-            for (int i = 0; i < size; i++) {
-                printf("%d ", list[i]);
-            }
-            printf("\n");
-            printf("Введите следующую команду: ");
-            opcode = getchar();
-        } 
-        else {
-            printf("Такой команды нет.");
-            printf("Введите следующую команду: ");
-            opcode = getchar();
+            scanf("%d", &val);
+            while (getchar() != '\n');
+            pop(&head, val);
+            break;
+        case 3:
+            printlist(head);
+            break;
+        default:
+            printf("Такой команды нет.\n");
+            break;
         }
+        printf("Введите следующую команду: ");
+    } while (opcode != 0);
+    
+    Node* cur = head;
+    Node* nextNode;
+    while (cur != NULL) {
+        nextNode = cur->next;
+        free(cur);
+        cur = nextNode;
     }
-
-    free(list);
+    head = NULL;
+    
     return 0;
 }
