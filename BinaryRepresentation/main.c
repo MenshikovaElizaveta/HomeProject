d45a177939b7c599c765b985a51f77c108d0c239
Elizaveta M <mefmag01@gmail.com>
2025-12-25 21:17:15 +0000

    Двоичное представление

---
+++ BinaryRepresentation/main.c
@@ -0,0 +1,39 @@
+#include "numsys.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+bool test_simple();
+bool test_negative_positive();
+bool test_reverse();
+bool test_zero();
+bool run_tests();
+
+int main(int argc, char* argv[])
+{
+    if (argc == 2 && strcmp(argv[1], "--test") == 0) {
+        if (!run_tests()) {
+            printf("The tests failed.\n");
+            return 1;
+        }
+        return 0;
+    }
+
+    int8_t num1, num2, sumdec;
+    char bin1[9], bin2[9], sumbin[9];
+
+    printf("Enter two numbers between -128 and 127: ");
+    scanf("%d %d", &num1, &num2);
+
+    binary_sys(num1, bin1);
+    binary_sys(num2, bin2);
+    add_binary_num(bin1, bin2, sumbin);
+    sumdec = decimal_sys(sumbin);
+
+    printf("These numbers are in binary notation: %s %s\n", bin1, bin2);
+    printf("Their sum is in binary form: %s\n", sumbin);
+    printf("Their sum in decimal notation: %d", sumdec);
+
+    return 0;
+}
---
+++ BinaryRepresentation/tests/test_num.c
@@ -0,0 +1,96 @@
+#include "../numsys.h"
+#include <stdbool.h>
+
+bool test_simple()
+{
+    int8_t num1 = 5;
+    int8_t num2 = 3;
+    int8_t sumdec;
+    char bin1[9], bin2[9], sumbin[9];
+
+    binary_sys(num1, bin1);
+    binary_sys(num2, bin2);
+
+    bool res1 = (strcmp(bin1, "00000101") == 0) && (strcmp(bin2, "00000011") == 0);
+
+    add_binary_num(bin1, bin2, sumbin);
+
+    bool res2 = (strcmp(sumbin, "00001000") == 0);
+
+    sumdec = decimal_sys(sumbin);
+
+    bool res3 = (sumdec == 8);
+
+    return res1 && res2 && res3;
+}
+
+bool test_negative_positive()
+{
+    int8_t num1 = 10;
+    int8_t num2 = -7;
+    int8_t sumdec;
+    char bin1[9], bin2[9], sumbin[9];
+
+    binary_sys(num1, bin1);
+    binary_sys(num2, bin2);
+
+    bool res1 = (strcmp(bin1, "00001010") == 0) && (strcmp(bin2, "11111001") == 0);
+
+    add_binary_num(bin1, bin2, sumbin);
+
+    bool res2 = (strcmp(sumbin, "00000011") == 0);
+
+    sumdec = decimal_sys(sumbin);
+
+    bool res3 = (sumdec == 3);
+
+    return res1 && res2 && res3;
+}
+
+bool test_reverse()
+{
+    int8_t num1 = 1;
+    int8_t num2 = -1;
+    int8_t sumdec;
+    char bin1[9], bin2[9], sumbin[9];
+
+    binary_sys(num1, bin1);
+    binary_sys(num2, bin2);
+
+    bool res1 = (strcmp(bin1, "00000001") == 0) && (strcmp(bin2, "11111111") == 0);
+
+    add_binary_num(bin1, bin2, sumbin);
+
+    bool res2 = (strcmp(sumbin, "00000000") == 0);
+
+    sumdec = decimal_sys(sumbin);
+
+    bool res3 = (sumdec == 0);
+
+    return res1 && res2 && res3;
+}
+
+bool test_zero()
+{
+    int8_t num1 = 0;
+    int8_t num2 = 0;
+    int8_t sumdec;
+    char bin1[9], bin2[9], sumbin[9];
+
+    binary_sys(num1, bin1);
+    binary_sys(num2, bin2);
+
+    bool res1 = (strcmp(bin1, "00000000") == 0) && (strcmp(bin2, "00000000") == 0);
+
+    add_binary_num(bin1, bin2, sumbin);
+
+    bool res2 = (strcmp(sumbin, "00000000") == 0);
+
+    sumdec = decimal_sys(sumbin);
+
+    bool res3 = (sumdec == 0);
+
+    return res1 && res2 && res3;
+}
+
+bool run_tests() { return test_simple() && test_negative_positive() && test_reverse() && test_zero(); }
